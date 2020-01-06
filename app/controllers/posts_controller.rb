cffeab58a878b9879545c3c6ae8c476b2d03360d
Sreejitha <sreejitha.vijayan@gmail.com>
2020-01-05 23:09:49 -0800

    added methods to controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,24 @@
 class PostsController < ApplicationController
 
- def index
- end
+  def index
+  end
 
- def new
- end
+  def new
+    @post = Post.new
+  end
 
- def create
- end
+  def create
+    @post = Post.new(post_params)
 
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
