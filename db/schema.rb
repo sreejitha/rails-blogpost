4cb750404986c5baf0126879d4ac676164b2f38e
Sreejitha <sreejitha.vijayan@gmail.com>
2020-01-14 22:44:49 -0800

    adding controller, model, route  and partial form for comments.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_06_063812) do
+ActiveRecord::Schema.define(version: 2020_01_10_065514) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "comment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
